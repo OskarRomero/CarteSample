b96b3627d5ff105dd89d3f6a9118daecc6a505be
Oscar Romero <108951818+OskarRomero@users.noreply.github.com>
2022-10-28 19:40:14 -0700

    Add files via upload

---
+++ dotNet/ICartService.cs
@@ -0,0 +1,21 @@
+﻿using Sabio.Models.Domain;
+using Sabio.Models.Requests;
+using System.Collections.Generic;
+
+namespace Sabio.Services.Interfaces
+{
+    public interface ICartService
+    {
+        int Add(CartAddRequest model);
+        public Cart GetCartById(int id);
+        public List<CartItem> GetAllByCreatedBy(int createdById);
+
+        public void DeleteById(int cartItemId);
+        public void DeleteByCreatedBy(int createdById);
+
+        public void UpdateCart(CartUpdateRequest model);
+
+        public List<CartPreview> GetRandomMenuItems();
+
+    }
+}
