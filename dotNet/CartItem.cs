56ec4c06b0dd03f4e50b02833b2f859a21101ae6
Oscar Romero <108951818+OskarRomero@users.noreply.github.com>
2022-10-28 18:57:55 -0700

    Add files via upload

---
+++ dotNet/CartItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int MenuItemId { get; set; }
+        public int Quantity { get; set; }
+        public string MenuItemName { get; set; }
+        public string MenuItemDescription { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal UnitCost { get; set; }
+        public int OrganizationId { get; set; }
+        public string OrganizationName { get; set; }
+        public int LocationId { get; set; }
+        public string LocationZip { get; set; }
+        public List<IngredientWithWarningRestriction> Ingredients { get; set; }
+        public List<LookUp> FoodSafeTypes { get; set; }     
+    }
+}
