7aaefd9f3e8947bc03e99f1b8a2bd5b51f2ca408
Oscar Romero <108951818+OskarRomero@users.noreply.github.com>
2023-02-23 09:43:18 -0800

    Update dbo.Cart_Select_ByCreatedBy.StoredProcedure.sql

--- SQL/dbo.Cart_Select_ByCreatedBy.StoredProcedure.sql
+++ SQL/dbo.Cart_Select_ByCreatedBy.StoredProcedure.sql
@@ -2,27 +2,32 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author: <Oscar Romero>
--- Description: <A Proc to select from dbo.Cart by CreatedBy or user Id>
+-- Create date: 
+-- Description: <A Proc to reads a record from dbo.Cart by user Id. 
 -- Code Reviewer:
 
--- MODIFIED BY: Oscar Romero
+-- MODIFIED BY: 
 -- MODIFIED DATE:
 -- Code Reviewer:
 -- Note:
--- =============================================
-CREATE proc [dbo].[Cart_Select_ByCreatedBy]
-		@CreatedBy int
-as
-/*              Test Code
-		Declare @CreatedBy int = 1
 
-		Execute [dbo].[Cart_Select_ByCreatedBy]
+-- MODIFIED BY: 
+-- MODIFIED DATE:
+-- Code Reviewer: 
+-- Note:
+-- =============================================
+ALTER proc [dbo].[Cart_Select_ByCreatedBy]
+				@CreatedBy int
+as
+/*  
+		Declare @CreatedBy int = 164
+		Execute [dbo].[Cart_Select_ByCreatedByV2]
 				@CreatedBy
 */
 BEGIN
-	--c=Cart mi=menuItem o=organizations l=location
 	SELECT 
 		 c.[Id] 
 		,c.[MenuItemId]
@@ -37,54 +42,59 @@ BEGIN
 		,l.Zip as LocationZip
 		,Ingredients = 
 			(
-			SELECT  i.Id
-				,i.Name
-				,i.ImageUrl
-				,i.measure 
-				,FoodWarningTypes = 
+			SELECT   i.Id
+					,i.Name
+					,i.ImageUrl
+					,i.measure 
+					,FoodWarningTypes = 
 					(
-					SELECT fwt.Id
-					      ,fwt.name
-					FROM dbo.FoodWarningTypes as fwt 
-					inner join dbo.IngredientWarnings as iw	on fwt.Id = iw.IngredientId 
-					WHERE iw.IngredientId = i.Id
-					FOR JSON AUTO
+						SELECT	 fwt.Id
+								,fwt.name
+						FROM dbo.FoodWarningTypes as fwt 
+							inner join dbo.IngredientWarnings as iw	on fwt.Id = iw.FoodWarningTypeId 
+						WHERE iw.IngredientId = i.Id
+						FOR JSON AUTO
 					)
-				,PurchaseRestrictions = 
-					(
-					SELECT pr.Id
-					      ,pr.Name
-					FROM dbo.PurchaseRestrictions as pr 
-						inner join dbo.Ingredients as i	on i.RestrictionId = pr.Id 
-						inner join dbo.MenuItemIngredients as mii on mii.IngredientId = i.Id
-						WHERE mii.CreatedBy = @CreatedBy
-					FOR JSON AUTO
-					)
+					,PurchaseRestrictions = 
+						(
+						SELECT   pr.Id
+								,pr.Name
+						FROM dbo.PurchaseRestrictions as pr 
+						WHERE pr.Id = i.RestrictionId
+						FOR JSON AUTO
+						)
 			FROM dbo.Ingredients as i 
 				inner join dbo.MenuItemIngredients as mii on i.Id = mii.IngredientId
-				inner join dbo.MenuItems as mi on mii.MenuItemId = mi.Id
-			WHERE mi.Id = mii.MenuItemId AND i.IsInStock = 1 AND mi.IsDeleted = 0
+			WHERE mii.MenuItemId=mi.Id 
+			--AND i.IsInStock = 1 AND mi.IsDeleted = 0
 			FOR JSON AUTO
-		    )
+			)
 		,FoodSafeTypes = 
 			(
 			SELECT
 				 fst.Id
 				,fst.Name
 			FROM dbo.FoodSafeTypes as fst 
-			inner join dbo.DietaryRestrictions as dr on dr.FoodSafeTypeId = fst.Id 
-			inner join dbo.MenuItems as mi on mi.Id = dr.MenuItemId
-			WHERE mi.CreatedBy = @CreatedBy
-			For JSON AUTO								
+				inner join dbo.DietaryRestrictions as dr on dr.FoodSafeTypeId = fst.Id 
+			WHERE dr.MenuItemId = mi.Id
+			FOR JSON AUTO								
 			)
+		,c.CustomerNotes
+		/*,Modifications = 
+			(
+			SELECT	 mm.Id
+					,mm.Count
+					,mm.CostChange
+			FROM dbo.MenuModifications as mm
+			WHERE mm.EntityId = c.Id
+			FOR JSON AUTO
+			)*/
 			  		  
 	FROM [dbo].[Cart] as c 
-	inner join dbo.MenuItems as mi on c.MenuItemId = mi.Id
-	inner join dbo.Organizations as o on mi.OrganizationId = o.Id
-	inner join dbo.Locations as l on o.PrimaryLocationId = l.Id 
-		  
-	WHERE c.CreatedBy = @CreatedBy
+		inner join dbo.MenuItems as mi on c.MenuItemId = mi.Id
+		inner join dbo.Organizations as o on mi.OrganizationId = o.Id
+		inner join dbo.Locations as l on o.PrimaryLocationId = l.Id 
+	Where c.CreatedBy = @CreatedBy
 
 		
 END
-GO
